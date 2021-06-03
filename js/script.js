6b09dc2acb6402cd7e816ee8399ad4ddd2d3b488
Chebanro2807 <Chebanro2807@ukr.net>
2021-06-03 16:31:17 +0300

    working with media

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
 const swiper = new Swiper('.tour-section .swiper-container', {
     loop: true,
-    slidesPerView:4,
+    slidesPerView:1,
     spaceBetween: 30,
     centeredSlider:true,
+    breakpoints:{
+        640:{
+            slidesPerView:1,
+            spaceBetween: 10,
+        },
+        768:{
+            slidesPerView:2,
+            spaceBetween: 20,
+        },
+        1024:{
+            slidesPerView:4,
+            spaceBetween: 30,
+        },
+    }
 });
 
 const swiper2 = new Swiper('.testimonials-section .swiper-container', {
